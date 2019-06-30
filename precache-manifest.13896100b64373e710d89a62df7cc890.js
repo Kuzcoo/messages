3bdc521511e7102ee395c10dadf7af8c0b6c9543
Thomas <tgerard79@yahoo.fr>
2019-06-30 12:11:01 +0200

    Updates

---
+++ precache-manifest.13896100b64373e710d89a62df7cc890.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21e95adcb6a54a27b52c42f7b9939a2",
+    "url": "/messages/index.html"
+  },
+  {
+    "revision": "78bf93a2467f4bef982b",
+    "url": "/messages/static/css/main.d1e45c0e.chunk.css"
+  },
+  {
+    "revision": "868e690876237246af77",
+    "url": "/messages/static/js/2.f7a11812.chunk.js"
+  },
+  {
+    "revision": "78bf93a2467f4bef982b",
+    "url": "/messages/static/js/main.428aa59c.chunk.js"
+  },
+  {
+    "revision": "52317be4d41582f07847",
+    "url": "/messages/static/js/runtime~main.18affc62.js"
+  }
+]);
